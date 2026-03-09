cd4a8bab6ad0aa2eca5632f616835b5d321f35d0
cjw9595 <jiaweichen9595@gmail.com>
2026-03-09 12:24:30 +0800

    添加了第一个关于折扣的单元测试，TDD-red

---
+++ app/src/main/java/com/example/demo/domain/discount/DiscountType.java
@@ -0,0 +1,6 @@
+package com.example.demo.domain.discount;
+
+public enum DiscountType {
+  FULL_REDUCTION, // 满减
+
+}
---
+++ app/src/test/java/com/example/demo/domain/order/PricingTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain.order;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.example.demo.domain.discount.DiscountType;
+import com.example.demo.domain.discount.FullReductionDiscount;
+import com.example.demo.domain.dish.DishId;
+
+public class PricingTest {
+
+  @Test
+  void order_should_calculate_correct_price_when_have_a_discount_and_meet_threshold_amount() {
+    FullReductionDiscount discount = new FullReductionDiscount(DiscountType.FULL_REDUCTION, new BigDecimal("50"),
+        new BigDecimal("5"));
+
+    OrderItem item1 = new OrderItem(new DishId("dish1"), "Dish 1", 2, new BigDecimal("30"));
+    List<OrderItem> items = List.of(item1);
+    Pricing pricing = Pricing.calculate(items, discount);
+
+    assertThat(pricing.itemsTotal()).isEqualByComparingTo(new BigDecimal("60.00"));
+    assertThat(pricing.packagingFee()).isEqualByComparingTo(Pricing.PACKAGING_FEE);
+    assertThat(pricing.deliveryFee()).isEqualByComparingTo(Pricing.DELIVERY_FEE);
+    assertThat(pricing.finalAmount()).isEqualByComparingTo(new BigDecimal("54.00"));
+
+    assertThat(pricing.discountInfo().applied()).isEqualTo(true);
+    assertThat(pricing.discountInfo().discountType()).isEqualTo("满减");
+    assertThat(pricing.discountInfo().amount()).isEqualByComparingTo(new BigDecimal("5.00"));
+  }
+
+}
