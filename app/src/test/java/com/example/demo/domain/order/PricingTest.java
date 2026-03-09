e5ed3f3b4b54f834a6689fed1e6482bab7ec730f
cjw9595 <jiaweichen9595@gmail.com>
2026-03-09 16:55:55 +0800

    修改测试名，添加菜品金额不达门槛时的测试

--- app/src/test/java/com/example/demo/domain/order/PricingTest.java
+++ app/src/test/java/com/example/demo/domain/order/PricingTest.java
@@ -13,7 +13,7 @@ import com.example.demo.domain.dish.DishId;
 public class PricingTest {
 
   @Test
-  void order_should_calculate_correct_price_when_have_a_discount_and_meet_threshold_amount() {
+  void order_should_calculate_correct_final_amouont_when_have_a_discount_and_meet_threshold_amount() {
     FullReductionDiscount discount = new FullReductionDiscount(new BigDecimal("50"),
         new BigDecimal("5"));
 
@@ -26,14 +26,10 @@ public class PricingTest {
     assertThat(pricing.deliveryFee()).isEqualByComparingTo(Pricing.DELIVERY_FEE);
     assertThat(pricing.finalAmount()).isEqualByComparingTo(new BigDecimal("59.00"));
 
-    // assertThat(pricing.discountInfo().applied()).isEqualTo(true);
-    // assertThat(pricing.discountInfo().discountType()).isEqualTo("满减");
-    // assertThat(pricing.discountInfo().amount()).isEqualByComparingTo(new
-    // BigDecimal("5.00"));
   }
 
   @Test
-  void order_should_calculate_correct_price_when_have_a_discount_and_just_meet_threshold_amount() {
+  void order_should_calculate_correct_final_amouont_when_have_a_discount_and_just_meet_threshold_amount() {
     FullReductionDiscount discount = new FullReductionDiscount(new BigDecimal("50"),
         new BigDecimal("5"));
 
@@ -45,6 +41,22 @@ public class PricingTest {
     assertThat(pricing.packagingFee()).isEqualByComparingTo(Pricing.PACKAGING_FEE);
     assertThat(pricing.deliveryFee()).isEqualByComparingTo(Pricing.DELIVERY_FEE);
     assertThat(pricing.finalAmount()).isEqualByComparingTo(new BigDecimal("49.00"));
+  }
+
+  @Test
+  void final_amouont_should_no_change_when_have_a_discount_but_not_meet_threshold_amount() {
+    FullReductionDiscount discount = new FullReductionDiscount(new BigDecimal("100"),
+        new BigDecimal("5"));
+
+    OrderItem item1 = new OrderItem(new DishId("dish1"), "Dish 1", 2, new BigDecimal("30"));
+    List<OrderItem> items = List.of(item1);
+    Pricing pricing = Pricing.calculate(items, discount);
+
+    assertThat(pricing.itemsTotal()).isEqualByComparingTo(new BigDecimal("60.00"));
+    assertThat(pricing.packagingFee()).isEqualByComparingTo(Pricing.PACKAGING_FEE);
+    assertThat(pricing.deliveryFee()).isEqualByComparingTo(Pricing.DELIVERY_FEE);
+    assertThat(pricing.finalAmount()).isEqualByComparingTo(new BigDecimal("64.00"));
+
   }
 
 }
